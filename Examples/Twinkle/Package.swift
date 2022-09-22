da249f3652df48c2b3dd88299aeeb5618a77b869
Andy Liu <andy@madmachine.io>
2022-09-22 08:18:50 +0000

    Modify example dependency
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/Twinkle/Package.swift
+++ Examples/Twinkle/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,9 +7,9 @@ let package = Package(
     name: "Twinkle",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", from: "0.0.1"),
-        .package(url: "https://github.com/madmachineio/MadBoards.git", from: "0.0.1"),
-        .package(path: "../../"),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/PWMTone.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

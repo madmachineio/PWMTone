212fb2799703c6657e9feb1421eea9778835f5af
Andy Liu <andy@madmachine.io>
2024-09-24 17:14:19 +0700

    Update for Embedded Swift
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "PWMTone",
             dependencies: ["SwiftIO"]),
-        .testTarget(
-            name: "PWMToneTests",
-            dependencies: ["PWMTone"]),
     ]
 )
